760ff9f15a051f1f8e75a38a46f01ebaf71224c1
Danila Danko <br4ch1st0chr0n3@gmail.com>
2023-06-17 15:18:20 +0300

    rm unused classes

--- src/Classes.purs
+++ src/Classes.purs
@@ -5,14 +5,8 @@ import Halogen (ClassName(..))
 cBound :: ClassName
 cBound = ClassName "bound"
 
-cButtonSettings :: ClassName
-cButtonSettings = ClassName "button-settings"
-
 cButtonSquare :: ClassName
 cButtonSquare = ClassName "button-square"
-
-cControls :: ClassName
-cControls = ClassName "controls"
 
 cCorrect :: ClassName
 cCorrect = ClassName "correct"
@@ -23,26 +17,8 @@ cCounter = ClassName "counter"
 cExercise :: ClassName
 cExercise = ClassName "exercise"
 
-cExerciseCounter :: ClassName
-cExerciseCounter = ClassName "exercise-counter"
-
-cFixed :: ClassName
-cFixed = ClassName "fixed"
-
 cHeader :: ClassName
 cHeader = ClassName "header"
-
-cHeaderCol :: ClassName
-cHeaderCol = ClassName "header-col"
-
-cIcon :: ClassName
-cIcon = ClassName "icon"
-
-cIconSettingsGear :: ClassName
-cIconSettingsGear = ClassName "icon-settings-gear"
-
-cIconSquare :: ClassName
-cIconSquare = ClassName "icon-square"
 
 cIncorrect :: ClassName
 cIncorrect = ClassName "incorrect"
@@ -53,14 +29,11 @@ cKeyboard = ClassName "keyboard"
 cKeyboardCol :: ClassName
 cKeyboardCol = ClassName "keyboard-col"
 
-cOperand :: ClassName
-cOperand = ClassName "operand"
+cSelect :: ClassName
+cSelect = ClassName "select"
 
-cOperator :: ClassName
-cOperator = ClassName "operator"
-
-cOperatorComparison :: ClassName
-cOperatorComparison = ClassName "operator-comparison"
+cSelected :: ClassName
+cSelected = ClassName "selected"
 
 cSettings ∷ ClassName
 cSettings = ClassName "settings"
